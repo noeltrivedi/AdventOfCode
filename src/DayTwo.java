8be17dd843242720f5cec99a8bfdedea30b7aae0
Noel Trivedi <nttrived@usc.edu>
2015-12-18 22:02:28 -0800

    Finished Day 2 Part 1

--- src/DayTwo.java
+++ src/DayTwo.java
@@ -2,11 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class DayTwo
 {
 	private String inputName = "day2input.txt";
+	private ArrayList<Integer[]> dimensions = new ArrayList<Integer[]>();
 	
 	public static void main(String args[])
 	{
@@ -16,6 +18,43 @@ public class DayTwo
 	public void solve()
 	{
 		readFile();
+		int soln = 0;
+		
+		//findSurfaceArea(dimensions.get(0));
+		
+		for(Integer[] input : dimensions)
+		{
+			soln += findSurfaceArea(input);
+		}
+		
+		
+		System.out.println(soln);
+	}
+	
+	private int findSurfaceArea(Integer[] dim)
+	{
+		int surfaceArea = 0;
+		
+		//Find the surface area of each face
+		for(int i = 0; i < dim.length; i++)
+		{
+			for(int j = i; j < dim.length; j++)
+			{
+				if(i != j)
+				{
+					//multiply by 2 because there are two faces
+					surfaceArea += 2*(dim[i] * dim[j]);
+				}
+			}
+		}
+		
+		//Find smallest face
+		int max = Math.max(dim[0], Math.max(dim[1], dim[2]));
+		int smallestFace = dim[0] * dim[1] * dim[2] / max;
+		
+		surfaceArea += smallestFace;
+		
+		return surfaceArea;
 	}
 	
 	private void readFile()
@@ -27,19 +66,25 @@ public class DayTwo
 			String line = null;
 			while((line = br.readLine()) != null)
 			{
-				System.out.println(line);
+				String[] dimStrings = line.split("x"); //Format is axbxc so this will make
+														//{'a','b','c'}
+				Integer[] dim = new Integer[3];
+				for(int i = 0; i < dimStrings.length; i++)
+				{
+					dim[i] = Integer.parseInt(dimStrings[i]);
+				}
+				
+				dimensions.add(dim);
 			}
 			br.close();
 			
 		}
 		catch (FileNotFoundException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		catch (IOException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
