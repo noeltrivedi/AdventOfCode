5d76aaaca154fc8c7d17ec5766e70056b46f4511
Noel Trivedi <nttrived@usc.edu>
2015-12-19 15:54:55 -0800

    Added comments which describe the problem

--- src/DayThree.java
+++ src/DayThree.java
@@ -4,7 +4,28 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
 
-
+/* Part One:
+ * Santa is delivering presents to an infinite two-dimensional grid of houses. He begins by 
+ * delivering a present to the house at his starting location, and then an elf at the North Pole 
+ * calls him via radio and tells him where to move next. Moves are always exactly one house to the 
+ * north (^), south (v), east (>), or west (<). After each move, he delivers another present to the 
+ * house at his new location.
+ * 
+ * However, the elf back at the north pole has had a little too much eggnog, and so his directions are 
+ * a little off, and Santa ends up visiting some houses more than once. 
+ * 
+ * How many houses receive at least one present?
+ * 
+ * Part Two:
+ * The next year, to speed up the process, Santa creates a robot version of himself, Robo-Santa, to 
+ * deliver presents with him.
+ * 
+ * Santa and Robo-Santa start at the same location (delivering two presents to the same starting house), 
+ * then take turns moving based on instructions from the elf, who is eggnoggedly reading from the same 
+ * script as the previous year. 
+ * 
+ * This year, how many houses receive at least one present?
+ */
 public class DayThree
 {
 	private String inputFile = "day3input.txt";
